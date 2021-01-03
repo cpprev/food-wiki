06acc6470404d81e518e928a3e1a26dccdfeda89
Kearnorent <charles.duchamp@epita.fr>
2021-01-03 19:03:50 +0100

    add database files

---
+++ src_back/DataBase/DataBase/DataBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBase
+{
+	public abstract class DataBase
+	{
+		public abstract T GetElementById<T>(string id);
+
+		public abstract List<T> GetAllElements<T>();
+
+		public abstract bool Add<T>(T element);
+
+		public abstract bool RemoveById<T>(string id);
+	}
+}
+
