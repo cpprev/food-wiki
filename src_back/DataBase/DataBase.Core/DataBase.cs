d7bbcca17a5515f518281e6454bcbe30120ae23f
Kearnorent <charles.duchamp@epita.fr>
2021-01-04 01:48:42 +0100

    comments added

--- src_back/DataBase/DataBase.Core/DataBase.cs
+++ src_back/DataBase/DataBase.Core/DataBase.cs
@@ -6,18 +6,66 @@ namespace DataBase.Core
 {
 	public abstract class DataBase
 	{
-		public abstract Task<T> GetElementById<T>(string collectionName, string id);
-
-		public abstract Task<T> GetElementByProperty<T>(string collectionName, string propName, string propValue);
-
+		/// <summary>
+		/// Retreives all elements from a collection
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collectionName"></param>
+		/// <returns></returns>
 		public abstract Task<List<T>> GetAllElements<T>(string collectionName);
 
+		/// <summary>
+		/// Adds an elements in a collection
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collectionName"></param>
+		/// <param name="element"></param>
+		/// <returns></returns>
 		public abstract Task<bool> Add<T>(string collectionName, T element);
 
+		/// <summary>
+		/// Gets an element from a collection by its Id.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collectionName"></param>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		public abstract Task<T> GetElementById<T>(string collectionName, string id);
+
+		/// <summary>
+		/// Gets an element from a collection by a given property
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collectionName"></param>
+		/// <param name="propName"></param>
+		/// <param name="propValue"></param>
+		/// <returns></returns>
+		public abstract Task<T> GetElementByProperty<T>(string collectionName, string propName, string propValue);
+
+		/// <summary>
+		/// Removes an elements from a collection thanks to its Id
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collectionName"></param>
+		/// <param name="id"></param>
+		/// <returns></returns>
 		public abstract Task<bool> RemoveById<T>(string collectionName, string id);
 
+		/// <summary>
+		/// Removes an elements from a collection thanks to a given property
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collectionName"></param>
+		/// <param name="propName"></param>
+		/// <param name="propValue"></param>
+		/// <returns></returns>
 		public abstract Task<bool> RemoveByProperty<T>(string collectionName, string propName, string propValue);
 
+		/// <summary>
+		/// Removes all elements from a collection
+		/// </summary>
+		/// <param name="collectionName"></param>
+		/// <returns></returns>
 		public abstract Task Clear(string collectionName);
 	}
 }
