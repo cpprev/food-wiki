f9f71b7ba258e83275cc186117e968eb3585d99e
Kearnorent <charles.duchamp@epita.fr>
2021-01-04 02:41:54 +0100

    created bats to launch apis/ui

---
+++ lauchRestApis.bat
@@ -0,0 +1,5 @@
+REM Build the solution
+dotnet build
+
+REM Launch the apis
+dotnet run --project .\src_back\Controllers\Controllers.csproj
---
+++ launchPortal.bat
@@ -0,0 +1,3 @@
+cd src_front\calory-calculator
+
+ng serve --open
